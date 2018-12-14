e5929e7673a30cf495cb5ca02e9ee27c916ce79d
emanayman657 <36210449+emanayman657@users.noreply.github.com>
2018-12-14 18:50:53 +0200

    Add files via upload

---
+++ Sequence.h
@@ -0,0 +1,26 @@
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+#include <iostream>
+using namespace std;
+
+
+class Sequence
+{
+    public:
+        Sequence();
+        void set_seq(string seq1);
+        char* get_seq();
+        Sequence ( Sequence&rhs);
+        friend istream& operator>>(istream& in,Sequence *seq2);
+         friend char* Align(Sequence * s1, Sequence * s2);
+        friend bool operator ==(Sequence& s2);
+
+        virtual ~Sequence();
+
+    protected:
+        char* seq;
+
+    private:
+};
+
+#endif // SEQUENCE_H
